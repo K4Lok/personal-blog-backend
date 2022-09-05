fa814f7c72314cf7f7ecec4d4d11c82ef0ded828
ImK4Lok <samkalok107@gmail.com>
2022-09-06 01:35:54 +0800

    add basic setup and the welcome screen animation

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+const config = require('./config');
+const appRouter = require('./routes/router');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use('/static', express.static('public'));
+
+app.use(appRouter);
+
+app.listen(config.app.port);
---
+++ routes/router.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+const baseRouter = require('./base');
+
+router.use(baseRouter);
+
+module.exports = router;
